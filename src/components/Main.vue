8731bf8e831a2c1ea261b0731aecf4656c480faf
MiolaTomas <tomasmiola71@gmail.com>
2025-10-07 18:45:41 -0300

    layout

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,28 +1,36 @@
 <template>
-    <div class="top-button">
-        <button class="btn-add" @click="addItem">
-            <i class="fa-solid fa-plus"></i>
-            <span>Add</span>
-        </button>
-        <button class="btn-add" @click="addItem">
-            <i class="fa-solid fa-plus"></i>
-            <span>Add</span>
-        </button>
-        <button class="btn-add" @click="addItem">
-            <i class="fa-solid fa-plus"></i>
-            <span>Add</span>
-        </button>
-        <button class="btn-add" @click="addItem">
-            <i class="fa-solid fa-plus"></i>
-            <span>Add</span>
-        </button>
+    <div class="container">
+
+        <div class="crud-buttons">
+            <button class="btn-add" @click="addItem">
+                <i class="fa-solid fa-plus"></i>
+                <span>Añadir</span>
+            </button>
+            <button class="btn-add" @click="editItem">
+                <i class="fa-solid fa-plus"></i>
+                <span>Editar</span>
+            </button>
+            <button class="btn-add" @click="deleteItem">
+                <i class="fa-solid fa-plus"></i>
+                <span>Eliminar</span>
+            </button>
+        </div>
+
+        <div class="navigation-buttons">
+            <button>&lt;&lt;</button>
+            <button>&lt;</button>
+            <button>&gt;</button>
+            <button>&gt;&gt;</button>
+        </div>
     </div>
 
 </template>
 
 <style scoped>
-.top-button {
+.container {
     background-color: red;
+    display: flex;
     padding: 1em;
+    justify-content: space-between;
 }
 </style>
